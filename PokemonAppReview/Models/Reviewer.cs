ae0c27e972282b125d565c9f8880bbd1167d82f6
Suzan Masite <SuzanM@blts.co.za>
2023-05-23 14:06:04 +0200

    Added the models

---
+++ PokemonAppReview/Models/Reviewer.cs
@@ -0,0 +1,10 @@
+﻿namespace PokemonAppReview.Models
+{
+    public class Reviewer
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastkName { get; set; }
+        public ICollection<Review>Reviews { get; set; }
+    }
+}
